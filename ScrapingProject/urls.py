4e8750766eefee3b246227e7410983baed63c452
tky1645 <t.nishioka.enesys@gmail.com>
2021-08-02 22:08:42 +0900

    Django|app作成＆起動確認

--- ScrapingProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('CarpageScrapingApp/', include('CarpageScrapingApp.urls')),
-    path('admin/', admin.site.urls),
-]
